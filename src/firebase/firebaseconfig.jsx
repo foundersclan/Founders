8b13cf83b9395a92c94ebd07336c3f8c2216901c
Aman Monga <amanmonga000@gmail.com>
2025-07-14 23:53:44 +0530

    Added environment variables file

--- src/firebase/firebaseconfig.jsx
+++ src/firebase/firebaseconfig.jsx
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDwHPf_LfC-q_VZ8NUEKk0eLFuLWS74u04",
-  authDomain: "founders-6ed01.firebaseapp.com",
-  projectId: "founders-6ed01",
-  storageBucket: "founders-6ed01.firebasestorage.app",
-  messagingSenderId: "747836366338",
-  appId: "1:747836366338:web:9d3a3735676f0f7d3f911b",
-  measurementId: "G-B9CV5EJZLM"
+  apiKey: `${import.meta.env.VITE_API_KEY}`,
+  authDomain: `${import.meta.env.VITE_AUTH_DOMAIN}`,
+  projectId: `${import.meta.env.VITE_PROJECT_ID}`,
+  storageBucket: `${import.meta.env.VITE_STORAGE_BUCKET}`,
+  messagingSenderId: `${import.meta.env.VITE_MESSAGING_SENDER_ID}`,
+  appId:`${import.meta.env.VITE_APP_ID}`,
+  measurementId: `${import.meta.env.VITE_MEASUREMENT_ID}`
 };
 
 // Initialize Firebase
