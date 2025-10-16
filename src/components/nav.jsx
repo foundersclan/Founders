3a306f0cf0ed1b66e8dfb0c5d63ed078618f94f6
Aman Monga <amanmonga000@gmail.com>
2025-10-17 03:11:05 +0530

    Minor Fixes

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -24,7 +24,7 @@ export const NavBar = () => {
         <nav className=" flex justify-between items-center p-4 bg-transparent">
             <div className="flex items-center gap-10">
                 <span className="md:text-5xl text-2xl flex items-center gap-2 font-bold  text-yellow-500 ">
-                    <img src="public/assets/full logo.jpg" className="md:w-1/4 md:h-1/4 w-8 h-8" alt="" />
+                    <img src="assets/full logo.jpg" className="md:w-1/4 md:h-1/4 w-8 h-8" alt="" />
                     </span>
                 {/* <ul className="flex text-white gap-5 mt-4 ">
                     {
